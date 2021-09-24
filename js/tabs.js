9d58b2b1b22d0e035eedd661969bbba71f2a82f5
Anna Tykhonenko <annsed001@gmail.com>
2021-09-24 16:25:12 +0300

    два блока

---
+++ js/tabs.js
@@ -0,0 +1,11 @@
+(function($) {
+$(function() {
+  
+  $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+    $(this)
+      .addClass('active').siblings().removeClass('active')
+      .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+  });
+  
+});
+})(jQuery);
